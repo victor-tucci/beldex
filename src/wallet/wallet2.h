4407d4cca4cb31b57b0c73fe4790f8bdcf5d575d
victor-tucci <victor.tucci@beldex.io>
2023-04-07 19:09:20 +0530

    unlock_balance call for wallet_api fix

--- src/wallet/wallet2.h
+++ src/wallet/wallet2.h
@@ -725,7 +725,7 @@ private:
 
     // locked & unlocked balance of given or current subaddress account
     uint64_t balance(uint32_t subaddr_index_major, bool strict) const;
-    uint64_t unlocked_balance(uint32_t subaddr_index_major, bool strict, uint64_t *blocks_to_unlock , uint64_t *time_to_unlock ,uint8_t hf_version) const;
+    uint64_t unlocked_balance(uint32_t subaddr_index_major, bool strict, uint64_t *blocks_to_unlock = NULL, uint64_t *time_to_unlock = NULL ,uint8_t hf_version =17) const;
     // locked & unlocked balance per subaddress of given or current subaddress account
     std::map<uint32_t, uint64_t> balance_per_subaddress(uint32_t subaddr_index_major, bool strict) const;
     std::map<uint32_t, std::pair<uint64_t, std::pair<uint64_t, uint64_t>>> unlocked_balance_per_subaddress(uint32_t subaddr_index_major, bool strict,uint8_t hf_version) const;
