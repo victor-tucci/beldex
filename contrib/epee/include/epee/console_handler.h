2b8f032ce1910e755f976f4aa5fe2fd054936cdc
victor-tucci <victor.tucci@beldex.io>
2023-05-04 13:15:37 +0530

    logger rebranding

--- contrib/epee/include/epee/console_handler.h
+++ contrib/epee/include/epee/console_handler.h
@@ -49,8 +49,8 @@
 #endif
 #include "readline_suspend.h"
 
-#undef MONERO_DEFAULT_LOG_CATEGORY
-#define MONERO_DEFAULT_LOG_CATEGORY "console_handler"
+#undef BELDEX_DEFAULT_LOG_CATEGORY
+#define BELDEX_DEFAULT_LOG_CATEGORY "console_handler"
 
 namespace epee
 {
