d1b1d1762460a3e530c86ebd4774fdb118ed6191
victor-tucci <victor.tucci@beldex.io>
2023-06-12 16:10:32 +0530

    aes flags property setup for cn-turtle compilation

--- src/crypto/cn_turtle_hash.c
+++ src/crypto/cn_turtle_hash.c
@@ -191,9 +191,9 @@ extern int aesb_pseudo_round(const uint8_t *in, uint8_t *out, const uint8_t *exp
   } while (0)
 
 
-#ifdef HAS_INTEL_HW // ARCH x86, x86-64
+#if defined(__x86_64__) || defined(_M_X64)
 
-#include "cn_turtle_hash-intel.inl"
+#include "cn_turtle_hash-amd64.inl"
 
 #elif defined(__arm__) || defined(__aarch64__) // ARCH arm
 
