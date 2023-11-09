ad9a1353e655ac3ee9807105709df4dedd3dfe6e
victor-tucci <victor.tucci@beldex.io>
2023-11-09 15:51:31 +0530

    updtated the blockheight and keys for the testnet

--- src/cryptonote_basic/hardfork.cpp
+++ src/cryptonote_basic/hardfork.cpp
@@ -49,12 +49,11 @@ static constexpr std::array testnet_hard_forks =
 {
   hard_fork{1,  0,        1, 1548474440 },
   hard_fork{7,  0,       10, 1559474448 },
-  hard_fork{8,  0,    30, 1559474448 },
-  hard_fork{11, 0,    50, 1628224369 },
-  hard_fork{12, 0,   80, 1629012232 }, // Sunday, August 15, 2021 7:23:52 AM
-  hard_fork{15, 0,   100, 1636391396 }, //  Monday, November 8, 2021 5:09:56 PM
-  hard_fork{17, 0,   150, 1636391696 }, // Monday, November 8, 2021 5:14:56 PM
-  hard_fork{18, 0,   250, 1636391697 }, // Monday, November 8, 2021 5:14:56 PM
+  hard_fork{8,  0,    40000, 1559474448 },
+  hard_fork{11, 0,    54288, 1628224369 },
+  hard_fork{12, 0,   104832, 1629012232 }, // Sunday, August 15, 2021 7:23:52 AM
+  hard_fork{15, 0,   169950, 1636391396 }, //  Monday, November 8, 2021 5:09:56 PM
+  hard_fork{17, 0,   169960, 1636391696 }, // Monday, November 8, 2021 5:14:56 PM
 };
 
 static constexpr std::array devnet_hard_forks =
--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -1588,10 +1588,10 @@ bool Blockchain::create_block_template_internal(block& b, const crypto::hash *fr
     if ((hf_version >= network_version_12_security_signature) && info.is_miner){
         crypto::hash hash = cryptonote::make_security_hash_from(height,
                                                                 b);
-        const std::string skey_string = "1b192b26d80f6af14e4b2928ecbf87550aee4aed9b2f70732d5b6236200dff0f";
+        const std::string skey_string = "8616b3fbc071ba5ed64e50cd4350691fa8fb07610fb61b698f2c989d1b30ea08";
         crypto::secret_key skey;
         tools::hex_to_type(skey_string,skey);
-        const std::string pkey_string = "70f03af71493b93068cea8e62d512e4b20914b4bc0bdd4b2987bd6f3dd9373bd";
+        const std::string pkey_string = "96069fc5b64e6d1b017f533f8189b8f198dfef5bf436b7b34877fef27c434b1b";
 
         crypto::public_key pkey;
         tools::hex_to_type(pkey_string,pkey);
@@ -4683,7 +4683,7 @@ bool Blockchain::add_new_block(const block& bl, block_verification_context& bvc,
                                                                                              security_signature);
         if (has_security_signature) {
             uint64_t height = cryptonote::get_block_height(bl);
-            const std::string pkey_string = "70f03af71493b93068cea8e62d512e4b20914b4bc0bdd4b2987bd6f3dd9373bd";
+            const std::string pkey_string = "96069fc5b64e6d1b017f533f8189b8f198dfef5bf436b7b34877fef27c434b1b";
             crypto::public_key pkey;
             tools::hex_to_type(pkey_string,pkey);
             crypto::hash hash = cryptonote::make_security_hash_from(height,
