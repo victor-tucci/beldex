8f7bf6dcb5d0ea202a26897cf2d3d57c17e3c0ee
victor-tucci <victor.tucci@beldex.io>
2026-05-04 19:12:29 +0530

    fix daemon relay_tx rpc handling

--- src/rpc/core_rpc_server.cpp
+++ src/rpc/core_rpc_server.cpp
@@ -2359,6 +2359,7 @@ namespace cryptonote::rpc {
     PERF_TIMER(on_relay_tx);
 
     std::string status = "";
+    std::vector<std::pair<crypto::hash, cryptonote::blobdata>> relayed_txs;
     for (const auto &str: relay_tx.request.txids)
     {
       crypto::hash txid;
@@ -2371,10 +2372,12 @@ namespace cryptonote::rpc {
       cryptonote::blobdata txblob;
       if (m_core.get_pool().get_transaction(txid, txblob))
       {
+        m_core.get_pool().set_relayable({txid});
         cryptonote_connection_context fake_context{};
         NOTIFY_NEW_TRANSACTIONS::request r{};
         r.txs.push_back(txblob);
         m_core.get_protocol()->relay_transactions(r, fake_context);
+        relayed_txs.emplace_back(txid, std::move(txblob));
         //TODO: make sure that tx has reached other nodes here, probably wait to receive reflections from other nodes
       }
       else
@@ -2384,6 +2387,9 @@ namespace cryptonote::rpc {
         continue;
       }
     }
+
+    if (!relayed_txs.empty())
+      m_core.get_pool().set_relayed(relayed_txs);
 
     if (status.empty())
       status = STATUS_OK;
